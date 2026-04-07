864fa39b128f5891d83015cdfbe978c7b1d768ba
Dima Karpinskyi <dimakjkarpinski@gmail.com>
2026-04-07 19:50:39 +0200

    adfewq

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
-import { useQuery } from '@tanstack/react-query';
-import { Toaster } from 'react-hot-toast';
+import { useEffect, useState } from 'react';
+import { useQuery, keepPreviousData } from '@tanstack/react-query';
+import toast, { Toaster } from 'react-hot-toast';
 import ReactPaginate from 'react-paginate';
 import type { Movie } from '../../types/movie';
 import type { MovieSearchResponse } from '../../services/movieService';
@@ -22,13 +22,14 @@ export default function App() {
     queryFn: () => fetchMovies(query, page),
     enabled: query.length > 0,
     staleTime: 1000 * 60 * 5,
-    placeholderData: {
-      results: [],
-      total_results: 0,
-      total_pages: 0,
-      page: 1,
-    },
+    placeholderData: keepPreviousData,
   });
+
+  useEffect(() => {
+    if (!isLoading && !isError && query.length > 0 && data?.results.length === 0) {
+      toast.error('No movies found for your request.');
+    }
+  }, [data, isLoading, isError, query]);
 
   const handleSearch = (searchTerm: string) => {
     setQuery(searchTerm);
